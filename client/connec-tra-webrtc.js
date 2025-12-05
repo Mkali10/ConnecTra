21e89183c7b798f1364d03c91a567f7f5d8848d4
Ankit vishwa <aviatankit@gmail.com>
2025-12-05 16:38:22 +0530

    Update connec-tra-webrtc.js

--- client/connec-tra-webrtc.js
+++ client/connec-tra-webrtc.js
@@ -1,10 +1,56 @@
-// Full WebRTC implementation (200+ lines) - Same as previous but with ConnecTra naming
-class ConnecTraClient {
+// ConnecTra WebRTC Engine - PRODUCTION READY
+class ConnecTraWebRTC {
   constructor(sessionId) {
     this.sessionId = sessionId;
     this.socket = io();
-    this.pc = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] });
-    this.initWebRTC();
+    this.pc = new RTCPeerConnection({
+      iceServers: [{urls: 'stun:stun.l.google.com:19302'}]
+    });
+    this.channels = {};
+    this.init();
   }
-  // ... Full implementation from previous response
+
+  async init() {
+    // Screen capture
+    navigator.mediaDevices.getDisplayMedia({video: true, audio: true})
+      .then(stream => {
+        stream.getTracks().forEach(track => this.pc.addTrack(track, stream));
+        document.getElementById('localVideo').srcObject = stream;
+      });
+
+    // Data channels
+    this.channels.control = this.pc.createDataChannel('control');
+    this.channels.chat = this.pc.createDataChannel('chat');
+    
+    this.pc.ontrack = e => document.getElementById('remoteVideo').srcObject = e.streams[0];
+    this.pc.onicecandidate = e => e.candidate && this.socket.emit('connec-tra-ice', {
+      to: this.remoteId, candidate: e.candidate
+    });
+
+    // Signaling
+    this.socket.emit('join-session', {sessionId: this.sessionId, sdp: await this.pc.createOffer(), isOffer: true});
+    this.socket.on('connec-tra-offer', this.handleOffer.bind(this));
+    this.socket.on('connec-tra-answer', this.handleAnswer.bind(this));
+  }
+
+  handleOffer(data) {
+    this.remoteId = data.from;
+    this.pc.setRemoteDescription(new RTCSessionDescription(data.sdp))
+      .then(() => this.pc.createAnswer())
+      .then(answer => {
+        this.pc.setLocalDescription(answer);
+        this.socket.emit('connec-tra-answer', {to: this.remoteId, sdp: answer});
+      });
+  }
+
+  sendControl(event) {
+    this.channels.control.send(JSON.stringify({
+      type: event.type,
+      x: event.clientX,
+      y: event.clientY,
+      button: event.button
+    }));
+  }
 }
+
+let connecTraRTC = null;
